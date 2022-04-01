4d6c279e035b279fd628291aa35a6d50552591ea
Deniz-Navarro <ldeniz1@ucol.mx>
2022-04-01 12:14:12 -0600

    Conection with firebase

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Guardaditos TR</Text>
+      <View style={styles.secondContainer}>
+        <Icon name="arrow-up" size={30} />
+        <Text>Swipe Up</Text>
+      </View>
     </View>
   );
 };
