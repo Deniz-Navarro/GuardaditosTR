10805eff0832608b1949191389dcdd684bba25f8
Samuel <samuel16mendoza@gmail.com>
2022-05-19 00:38:29 -0500

    Add SplashScreen
    Add a SplashScreen to the Welcome screen for access without a button to the Login

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, Image} from 'react-native';
-import Button from '../components/atoms/Button';
 import LogoImg from '../assets/images/LogoUDC2.png';
 import styles from './styles';
 
 export const Welcome = ({navigation}) => {
+  const [cambiar, setCambiar] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCambiar(true);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    if (cambiar) {
+      navigation.replace('Login');
+    }
+  }, [cambiar, navigation]);
+
   return (
     <SafeAreaView style={styles.containerWelcome}>
       <Text style={styles.title}>Guardaditos</Text>
       <Text>By TimRocket</Text>
       <Image style={styles.tinyLogo} source={LogoImg} />
-      <Button
-        text="¡Comenzar!"
-        onPress={() => {
-          navigation.navigate('Login');
-        }}
-      />
     </SafeAreaView>
   );
 };
