c269399743035e1e63ca6c67148b70faf6a9706a
Nando San Miguel <contact@namoku.xyz>
2022-06-01 00:44:05 -0500

    Add google sing in and can be runned in iOS

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,16 +5,35 @@ import Button from '../components/atoms/Button';
 import auth from '@react-native-firebase/auth';
 import LogoImg from '../assets/images/LogoUDC2.png';
 import styles from './styles';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '95653926946-3vlr2lqcf2cph3graamr9em2mh6vlgdp.apps.googleusercontent.com',
+});
+
+const navigateToHome = navigation =>
+  navigation.reset({
+    index: 0,
+    routes: [{name: 'Menu'}],
+  });
+
+const onGoogleButtonPress = async navigation => {
+  const {idToken} = await GoogleSignin.signIn();
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  const authAux = await auth().signInWithCredential(googleCredential);
+  if (authAux.additionalUserInfo.profile.hd === 'ucol.mx') {
+    if (authAux.additionalUserInfo.isNewUser) {
+      console.log('aa');
+    }
+    return navigateToHome(navigation);
+  }
+};
 
 const authUser = (email, password, navigation) => {
   auth()
     .signInWithEmailAndPassword(email, password)
-    .then(() => {
-      navigation.reset({
-        index: 0,
-        routes: [{name: 'Menu'}],
-      });
-    })
+    .then(() => navigateToHome(navigation))
     .catch(error => {
       if (error.code === 'auth/operation-not-allowed') {
         console.log('Usuario o contraseña incorrectos');
@@ -51,6 +70,10 @@ export const Login = ({navigation}) => {
       <Button
         text="Iniciar sesion"
         onPress={() => authUser(email, pass, navigation)}
+      />
+      <Button
+        text="Iniciar sesion con Google"
+        onPress={() => onGoogleButtonPress(navigation)}
       />
       <Text
         style={styles.registro}
